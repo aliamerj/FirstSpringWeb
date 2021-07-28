c6cc99683bf0bcf7bc423047cebf844e0588c938
ali amer <76897266+joeamerson@users.noreply.github.com>
2021-07-28 21:00:50 +0300

    work with  class

--- src/main/java/first/ali/AliApplication.java
+++ src/main/java/first/ali/AliApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import first.ali.model.Students;
+
 @SpringBootApplication
 @RestController
 public class AliApplication {
@@ -14,9 +16,16 @@ public class AliApplication {
 	}
 
 
-	@GetMapping
+	@GetMapping(value = "/hello")
 	public String hello (){
 		return "Hello world";
+	}
+
+	@GetMapping(value = "/students")
+	public Students getStudents(){
+		var newStudent = new Students(123L,"ali","amer",21);
+		return newStudent;
+
 	}
 
 }
---
+++ src/main/java/first/ali/model/Students.java
@@ -0,0 +1,74 @@
+package first.ali.model;
+
+public class Students {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Students() {
+    }
+
+
+    public Students(Long id, String firstName, String lastName, int age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Students(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", firstName='" + getFirstName() + "'" +
+            ", lastName='" + getLastName() + "'" +
+            ", age='" + getAge() + "'" +
+            "}";
+    }
+
+
+
+    
+
+    
+}
