259337c26cf6f7ba0e19e10c8ca4294948d3dbc5
ali amer <76897266+joeamerson@users.noreply.github.com>
2021-07-28 18:54:26 +0300

    first commit

---
+++ src/main/java/first/ali/AliApplication.java
@@ -0,0 +1,22 @@
+package first.ali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AliApplication.class, args);
+	}
+
+
+	@GetMapping
+	public String hello (){
+		return "Hello world";
+	}
+
+}
